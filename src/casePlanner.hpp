ffc135cfeac20c2d61c8814367f66372091203bf
Kerseee <racs10030746@gmail.com>
2021-10-03 15:09:37 +0800

    Define genealogy of Experimentors

--- src/casePlanner.hpp
+++ src/casePlanner.hpp
@@ -7,31 +7,94 @@
 #include <ostream>
 #include <vector>
 #include "data.hpp"
-#include "caseData.hpp"
 #include "planner.hpp"
+#include "caseData.hpp"
+#include "caseGenerator.hpp"
 
 namespace planner {
 
 // Experimentor execute an experiment based on given data and random demands.
 class Experimentor {
 protected:
+    /* Variables */
     const data::CaseData* data_;    // A data copy
     State state_;                   // States in one experiment
     double revenue;
 
+    /* Protected methods */
     // isAcceptable check if this order is acceptable with or without upgrade    
     bool isAcceptable(const Order& order, const State& state);
 
+    // findBestRevOrder return the best revenue obtained from random order 
+    double findBestRevOrder(const int period, const Order& order);
+
+    virtual void findOptPlan() = 0;
+
+    // updateState update the given state with the given plan
+    void updateState(State& state, const MyopicUpgradePlan& plan);
+
+    // getRevDemand return the revenue of random demand from individuals
+    double getRevDemand(const State& demand);
+
 public:
+    /* Constructors */
     Experimentor();
     Experimentor(const data::CaseData& data);
 
+    /* Public methods */
     void reset();
     void run();
 };
 
-// 待決定是否需要這個
-class MyopicCasePlanner {
+// Deterministic planner
+class DeterExperimentor: public virtual Experimentor{
+protected:
+    // findOptPlan modify the given plan, considering the demand we guess. 
+    // This problem is formulated in an integer programming problem in this function.
+    void findOptPlan(const int period, MyopicUpgradePlan& plan, 
+                     const State& state, const State& demand,
+                     const std::map<data::tuple2d, double>& prices);
+public:
+    void run();
+};
+
+// Stochastic planner
+class StochExperimentor: public virtual Experimentor{
+protected:
+    // findOptPlan modify the given plan, considering the demand we guess. 
+    // This problem is formulated in an stochastic programming problem in this function.
+    void findOptPlan(const int period, MyopicUpgradePlan& plan, 
+                     const State& state, const std::vector<State>& s_demands,
+                     const std::vector<std::map<data::tuple2d, double> >& prices);
+
+public:
+    void run();
+
+};
+
+// Adjusted planner
+class AdjExperimentor: public virtual Experimentor {
+protected:
+    // findBaseline find the baseline in bs mode
+    double findBaseline(const int period, const MyopicUpgradePlan& plan, 
+                        const State& state);
+    
+    // decide decide whether to accept this plan (upgraded order) or not.
+    void decide(MyopicUpgradePlan& plan, double baseline);
+};
+
+class ADExperimentor: public AdjExperimentor, public DeterExperimentor {
+public:
+    void run();
+};
+
+class ASExperimentor: public AdjExperimentor, public StochExperimentor {
+public:
+    void run();
+};
+
+
+class ExperimentsController {
 
 };
 
